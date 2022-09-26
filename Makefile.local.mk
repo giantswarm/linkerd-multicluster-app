0c51ff9d248745995e054f9ba20ba7f173198cac
Matias Charriere <matias@giantswarm.io>
2022-09-26 11:09:37 +0200

    use old name for main chart

--- Makefile.local.mk
+++ Makefile.local.mk
@@ -4,7 +4,7 @@
 update-chart: ## Run vendir and patch upstream charts
 	@echo "====> $@"
 	vendir sync
-	rm -rf ./helm/linkerd-multicluster/charts
-	mkdir -p ./helm/linkerd-multicluster/charts
+	rm -rf ./helm/linkerd2-multicluster-app/charts
+	mkdir -p ./helm/linkerd2-multicluster-app/charts
 	cp -r ./vendor/linkerd-multicluster/multicluster/charts/linkerd-multicluster ./helm/linkerd-multicluster/charts/linkerd-multicluster
 	cp -r ./vendor/linkerd-multicluster/charts/partials ./helm/linkerd-multicluster/charts/partials
