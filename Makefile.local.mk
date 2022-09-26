84436d57b8d6608b7dd97f7bd946029c30e11882
Matias Charriere <matias@giantswarm.io>
2022-09-26 09:35:45 +0200

    correct makefile paths

--- Makefile.local.mk
+++ Makefile.local.mk
@@ -4,7 +4,7 @@
 update-chart: ## Run vendir and patch upstream charts
 	@echo "====> $@"
 	vendir sync -l
-	rm -rf ./helm/linkerd-multicluster/charts/linkerd-multicluster
-	rm -rf ./helm/linkerd-multicluster/charts/partials
+	rm -rf ./helm/linkerd-multicluster/charts
+	mkdir -p ./helm/linkerd-multicluster/charts
 	cp -r ./vendor/linkerd-multicluster/multicluster/charts/linkerd-multicluster ./helm/linkerd-multicluster/charts/linkerd-multicluster
 	cp -r ./vendor/linkerd-multicluster/charts/partials ./helm/linkerd-multicluster/charts/partials
