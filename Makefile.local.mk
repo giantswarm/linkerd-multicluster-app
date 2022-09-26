7bed4c2d7c76abca1008964e437b9b7adeac7e49
Matias Charriere <matias@giantswarm.io>
2022-09-26 10:15:00 +0200

    Upgrade to include GS changes

--- Makefile.local.mk
+++ Makefile.local.mk
@@ -3,7 +3,7 @@
 .PHONY: update-chart
 update-chart: ## Run vendir and patch upstream charts
 	@echo "====> $@"
-	vendir sync -l
+	vendir sync
 	rm -rf ./helm/linkerd-multicluster/charts
 	mkdir -p ./helm/linkerd-multicluster/charts
 	cp -r ./vendor/linkerd-multicluster/multicluster/charts/linkerd-multicluster ./helm/linkerd-multicluster/charts/linkerd-multicluster
